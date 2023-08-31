ac6d2ff238e74555159258b8e5a9518d7bbe4881
Mustapha Rabiu Adeola <konnect1795@gmail.com>
2023-08-31 13:28:13 +0100

    side&Top_NavBars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+ import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'newsdirect';
+
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private cdr : ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+     this.sideNav.opened = true;
+      this.observer.observe(['(max-width:787px)']).subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = 'over';
+          this.sideNav.close();
+        } else {
+          this.sideNav.mode = 'side';
+          this.sideNav.open();
+        }
+      });
+      this.cdr.detectChanges();
+  }
 }
